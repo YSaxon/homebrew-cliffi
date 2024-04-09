c970a61838afcf7d82abcd5b2f4561b004d34a7e
Yaakov Saxon <ysaxon@gmail.com>
2024-04-09 18:26:03 -0400

    cliffi 1.1.12
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/cliffi.rb
+++ Formula/cliffi.rb
@@ -1,8 +1,8 @@
 class Cliffi < Formula
   desc "Tool for calling shared library functions directly from your shell"
   homepage "https://github.com/YSaxon/cliffi"
-  url "https://github.com/YSaxon/cliffi/archive/refs/tags/v1.1.3.tar.gz"
-  sha256 "3aade8e40216c167116bd7d91614e29fcdc1c719fc7cf75e111ac81e4315e604"
+  url "https://github.com/YSaxon/cliffi/archive/refs/tags/v1.1.12.tar.gz"
+  sha256 "6b4e6a54b107b045dfe688fa26f8ef06bdee2aeb6575e2690be9460a203b2313"
   license "MIT"
 
   depends_on "cmake" => [:build, :test]
